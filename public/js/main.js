9ed6fcde4f50647d3550c7e6ac821dcc8185df73
Alan Williams <alanjosephwilliams@gmail.com>
2014-08-14 11:37:15 -0700

    added full page leaflet/mapbox map

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,11 @@
+var map = new L.map("map", {center: [37.783, -122.425], zoom: 13})
+  .addLayer( new L.TileLayer('http://{s}.tiles.mapbox.com/v3/alanjosephwilliams.j8058jhn/{z}/{x}/{y}.png'));
+
+// var map = new L.Map("map", {center: [37.8, -96.9], zoom: 4})
+//     .addLayer(new L.TileLayer("http://{s}.tiles.mapbox.com/v3/examples.map-vyofok3q/{z}/{x}/{y}.png"));
+
+
+
 function get_location() {
     navigator.geolocation.getCurrentPosition(on_success);
 }
@@ -5,7 +13,6 @@ function get_location() {
 function on_success(currentPosition) {
 	my_location = {};
 	my_location = { latitude: currentPosition.coords.latitude, longitude: currentPosition.coords.longitude }
-	console.log(my_location);
 }
 
 var my_location;
